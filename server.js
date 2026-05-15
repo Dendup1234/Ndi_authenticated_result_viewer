8c38259b682f61cd09076381730bf5b8e01ddb72
Dendup1234 <dendup091@gmail.com>
2026-05-15 14:36:44 +0600

    vps setup

--- server.js
+++ server.js
@@ -15,6 +15,6 @@ app.get('/', (req, res) => {
 
 await connectDB();
 
-app.listen(port, () => {
+app.listen(port,"0.0.0.0", () => {
     console.log(`Server running at http://localhost:${port}`);
 });
