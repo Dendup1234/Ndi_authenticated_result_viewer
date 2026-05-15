8bbb663dec0d2b6c9680e7d3cb1642dfcc9e91d7
Dendup1234 <dendup091@gmail.com>
2026-05-15 15:08:40 +0600

    vps testing

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const port = process.env.PORT ?? 3000;
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('Hello World Dendup is here!');
 });
 
 await connectDB();
